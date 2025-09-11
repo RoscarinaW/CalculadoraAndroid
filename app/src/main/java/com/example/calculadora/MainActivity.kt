a0012e431e826fed1a76e5b4f2a39cb9ee7da308
RoscarinaW <oscarinawrn@gmail.com>
2025-09-11 18:27:23 -0500

    Ya se agrega la funcion de seleccionar numeros

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.calculadora
 
 import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -36,5 +38,38 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        formatoDecimal = DecimalFormat("#.#######")
+        TvTempo = findViewById(R.id.TvTempo)
+        tvResul = findViewById(R.id.tvResul)
+    }
+
+    fun cambiarOperador(b: View) {
+        val boton: Button = b as Button
+        operacionActual = boton.text.toString().trim()
+    }
+    fun calcular(b: View) {
+        if(Double.NaN!=primerNumero){
+            segundoNumero = TvTempo.text.toString().toDouble()
+            TvTempo.text=""
+
+            when(operacionActual){
+                "+" -> primerNumero = (primerNumero+segundoNumero)
+                "-" -> primerNumero = (primerNumero-segundoNumero)
+                "*" -> primerNumero = (primerNumero*segundoNumero)
+                "/" -> primerNumero = (primerNumero/segundoNumero)
+                "%" -> primerNumero = (primerNumero%segundoNumero)
+            }
+        }else{
+             primerNumero = TvTempo.text.toString().toDouble()
+        }
+    }
+
+    fun  seleccionarNumero(b: View){
+        val boton: Button = b as Button
+        if(TvTempo.text.toString()=="0"){
+            TvTempo.text=""
+        }
+        TvTempo.text = TvTempo.text.toString() + boton.text.toString()
     }
 }
