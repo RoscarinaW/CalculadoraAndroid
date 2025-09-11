05b0aee3194b0f773cbf2161816f75e5b1f99802
RoscarinaW <oscarinawrn@gmail.com>
2025-09-11 17:17:09 -0500

    Se termina la part visual de la calculadora.

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.calculadora
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val SUMA = "+"
+    val RESTA = "-"
+    val MULTIPLICACION = "*"
+    val DIVISION = "/"
+    val PORCENTAJE = "%"
+
+    var operacionActual = ""
+
+    var primerNumero: Double = Double.NaN
+    var segundoNumero: Double = Double.NaN
+
+    lateinit var TvTempo: TextView
+    lateinit var tvResul: TextView
+
+    lateinit var formatoDecimal: DecimalFormat
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
